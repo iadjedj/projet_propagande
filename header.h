ad6354ae9725af191888f18fbfb304864c098d35
Ilann ADJEDJ <iadjedj@e1r2p10.42.fr>
2015-02-03 16:07:17 +0100

    Au Dieu que la clarte

--- header.h
+++ header.h
@@ -6,7 +6,7 @@
 /*   By: iadjedj <iadjedj@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/02 16:14:31 by iadjedj           #+#    #+#             */
-/*   Updated: 2015/02/02 16:15:40 by iadjedj          ###   ########.fr       */
+/*   Updated: 2015/02/03 15:51:33 by iadjedj          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <fcntl.h>
+# include <time.h>
 # define BUFF_SIZE 2048
 
 int	get_next_line(int const fd, char **line);
--- main.c
+++ main.c
@@ -6,27 +6,55 @@
 /*   By: iadjedj <iadjedj@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/02 16:06:11 by iadjedj           #+#    #+#             */
-/*   Updated: 2015/02/02 17:24:13 by iadjedj          ###   ########.fr       */
+/*   Updated: 2015/02/03 16:06:34 by iadjedj          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 #include "dico.c"
 
-#define MAX_WORD_LEN 15
+#define MAX_WORD_LEN 10
 
 char	*get_word(char *word)
 {
 	int		i;
+	// int		size;
+	// int		plur;
+	char **tab;
 
 	i = 0;
-	while (DICO[i] != NULL &&
-			ft_strncmp(DICO[i], word, ft_strlen(word)) != 0)
+	// plur = 0;
+	// size = strlen(word) + 1;
+	while (DICO[i] != NULL)
+	{
+		tab = ft_strsplit(DICO[i], '=');
+		if (ft_strequ(tab[0], word))
+		{
+			free(tab[0]);
+			return (ft_strdup(tab[1]));
+		}
+		else
+		{
+			free(tab[0]);
+			free(tab[1]);
+			free(tab);
+		}
+		// if (word[size - 2] == 's' && (word[size - 1] == '.' || word[size - 1] == ','))
+		// 	plur = 2;
+		// else if (word[size - 1] == 's' || word[size - 1] == '.' || word[size - 1] == ',')
+		// 	plur = 1;
+		// else	
+		// 	plur = 0;
 		i++;
-	if (DICO[i] == NULL)
+	}
+	// if (DICO[i] == NULL)
 		return (NULL);
-	else
-		return (DICO[i] + ft_strlen(word) + 1);
+	// else if (plur == 2)
+	// 	return (DICO[i] + strlen(word) - 1);
+	// else if (plur == 1)
+	// 	return (DICO[i] + strlen(word)/* + 1*/);
+	// else
+	// 	return (DICO[i] + strlen(word) + 1);
 }
 
 char		*find_syn(char *word)
@@ -41,7 +69,7 @@ char		*find_syn(char *word)
 	i = 0;
 	while (syn && syn[i])
 	{
-		if (ft_strlen(syn[i]) < ft_strlen(word))
+		if (strlen(syn[i]) < strlen(word))
 			return (syn[i]);
 		i++;
 	}
@@ -66,16 +94,44 @@ int main(int ac, char const **av)
 		i = 0;
 		while (tab[i])
 		{
-			if (ft_strlen(tab[i]) >= MAX_WORD_LEN)
-				ft_putstr("\033[31mCENSURE\033[0m");
-			else if ((tmp = find_syn(tab[i])) != NULL)
+			if (strlen(tab[i]) >= MAX_WORD_LEN)
+				// tab[i] = "";
+				ft_putstr("\033[31mCENSURE\033[0m ");
+			else if ((tmp = find_syn(tab[i])) != NULL && (int)strlen(tmp) < (rand() % 10) + 5)
+			{
+				// tab[i] = tmp;
+				ft_putstr("\033[32m");
 				ft_putstr(tmp);
-			else
+				ft_putstr("\033[0m");
+				ft_putchar(' ');
+			}
+			else if (strlen(tab[i]) <= 5)
+			{
+				// tab[i] = "";
 				ft_putstr(tab[i]);
-			ft_putchar(' ');
+				ft_putchar(' ');
+			}
 			free(tab[i]);
 			i++;
 		}
+		// i = 0;
+		// compt = 0;
+		// while (tab[i])
+		// {
+		// 	if (strlen(tab[i]) > MAX_WORD_LEN)
+		// 		compt++;
+		// 	i++;
+		// }
+		// i = 0;
+		// if (compt < 3)
+		// {
+		// 	while (tab[i])
+		// 	{
+		// 		ft_putstr(tab[i]);
+		// 		ft_putchar(' ');
+		// 		i++;
+		// 	}
+		// }
 		ft_putchar('\n');
 		free(tab);
 	}
