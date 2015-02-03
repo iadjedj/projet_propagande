da8c13a30c8eac8c9774c0edc3187af6a57717a3
Ilann ADJEDJ <iadjedj@e3r6p7.42.fr>
2015-02-03 21:10:33 +0100

    Optimisation algo recherche + petits ajustements

--- main.c
+++ main.c
@@ -6,55 +6,64 @@
 /*   By: iadjedj <iadjedj@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/02 16:06:11 by iadjedj           #+#    #+#             */
-/*   Updated: 2015/02/03 16:06:34 by iadjedj          ###   ########.fr       */
+/*   Updated: 2015/02/03 20:55:42 by iadjedj          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
-#include "dico.c"
+#include "dico.h"
+#include "freq.h"
 
-#define MAX_WORD_LEN 10
+#define MAX_WORD_LEN 12
 
 char	*get_word(char *word)
 {
 	int		i;
-	// int		size;
-	// int		plur;
-	char **tab;
+	int		j;
+	int		size;
 
 	i = 0;
-	// plur = 0;
-	// size = strlen(word) + 1;
+	size = strlen(word);
+	if (word[size - 1] == 's' || word[size - 1] == '.' || word[size - 1] == ',')
+	{
+		word[size - 1] = '\0';
+		size--;
+	}
 	while (DICO[i] != NULL)
 	{
-		tab = ft_strsplit(DICO[i], '=');
-		if (ft_strequ(tab[0], word))
+		j = 0;
+		while (word[j] && DICO[i][j] != '=' && DICO[i][j] == word[j])
 		{
-			free(tab[0]);
-			return (ft_strdup(tab[1]));
+			j++;
 		}
-		else
-		{
-			free(tab[0]);
-			free(tab[1]);
-			free(tab);
-		}
-		// if (word[size - 2] == 's' && (word[size - 1] == '.' || word[size - 1] == ','))
-		// 	plur = 2;
-		// else if (word[size - 1] == 's' || word[size - 1] == '.' || word[size - 1] == ',')
-		// 	plur = 1;
-		// else	
-		// 	plur = 0;
+		if (word[j] == '\0')
+			return (DICO[i]);
 		i++;
 	}
-	// if (DICO[i] == NULL)
-		return (NULL);
-	// else if (plur == 2)
-	// 	return (DICO[i] + strlen(word) - 1);
-	// else if (plur == 1)
-	// 	return (DICO[i] + strlen(word)/* + 1*/);
-	// else
-	// 	return (DICO[i] + strlen(word) + 1);
+	return (NULL);
+}
+
+float		get_freq(char *str)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	while (FREQ[i] != NULL)
+	{
+		j = 0;
+		while (str[j] && FREQ[i][j] != '\t' && FREQ[i][j] == str[j])
+		{
+			j++;
+		}
+		if (str[j] == '\0')
+			break;
+		i++;
+	}
+	if (FREQ[i] == NULL)
+		return (0);
+	else
+		return (atof(ft_strchr(FREQ[i], '\t')));
 }
 
 char		*find_syn(char *word)
@@ -62,18 +71,35 @@ char		*find_syn(char *word)
 	char			*tmp;
 	char			**syn;
 	int				i;
+	size_t			len;
 
 	if (!(tmp = get_word(word)))
 		return (NULL);
 	syn = ft_strsplit(tmp, ' ');
 	i = 0;
+	len = strlen(word);
 	while (syn && syn[i])
 	{
-		if (strlen(syn[i]) < strlen(word))
+		if (get_freq(syn[i]) > get_freq(word) && strlen(syn[i]) <= len)
+		{
 			return (syn[i]);
+		}
+		free(syn[i]);
 		i++;
 	}
+	free(syn);
 	return (word);
+}
+
+void	censure(char *mot)
+{
+	int		len;
+
+	len = (strlen(mot) % 12) + 2;
+	// ft_putstr("\033[31m");
+	while (len--)
+		ft_putstr("█");
+	ft_putstr("\033[0m ");
 }
 
 int main(int ac, char const **av)
@@ -95,45 +121,31 @@ int main(int ac, char const **av)
 		while (tab[i])
 		{
 			if (strlen(tab[i]) >= MAX_WORD_LEN)
-				// tab[i] = "";
-				ft_putstr("\033[31mCENSURE\033[0m ");
-			else if ((tmp = find_syn(tab[i])) != NULL && (int)strlen(tmp) < (rand() % 10) + 5)
+				censure(tab[i]);
+			else if ((tmp = find_syn(tab[i])) != NULL)
 			{
-				// tab[i] = tmp;
-				ft_putstr("\033[32m");
-				ft_putstr(tmp);
-				ft_putstr("\033[0m");
-				ft_putchar(' ');
+				if ((int)strlen(tmp) < (rand() % 10) + 5)
+				{
+					ft_putstr("\033[32m");
+					ft_putstr(tmp);
+					ft_putstr("\033[0m ");
+				}
+				else
+					censure(tab[i]);
 			}
-			else if (strlen(tab[i]) <= 5)
+			else if (strlen(tab[i]) > 0 || ft_isdigit(tab[i][0]))
 			{
-				// tab[i] = "";
 				ft_putstr(tab[i]);
 				ft_putchar(' ');
 			}
+			else
+				censure(tab[i]);
 			free(tab[i]);
 			i++;
 		}
-		// i = 0;
-		// compt = 0;
-		// while (tab[i])
-		// {
-		// 	if (strlen(tab[i]) > MAX_WORD_LEN)
-		// 		compt++;
-		// 	i++;
-		// }
-		// i = 0;
-		// if (compt < 3)
-		// {
-		// 	while (tab[i])
-		// 	{
-		// 		ft_putstr(tab[i]);
-		// 		ft_putchar(' ');
-		// 		i++;
-		// 	}
-		// }
 		ft_putchar('\n');
 		free(tab);
+		free(line);
 	}
 	return (0);
 }
